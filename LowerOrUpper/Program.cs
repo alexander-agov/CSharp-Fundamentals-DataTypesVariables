d7acf65d2b2fed02ef2f9414f141048f3d19a73c
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:26:33 +0300

    Add files via upload

---
+++ LowerOrUpper/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LowerOrUpper
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            char letter = char.Parse(Console.ReadLine());
+            if (letter == char.ToUpper(letter))
+            {
+                Console.WriteLine("upper-case");
+            } else 
+            {
+                Console.WriteLine("lower-case");
+             }
+        }
+    }
+}
