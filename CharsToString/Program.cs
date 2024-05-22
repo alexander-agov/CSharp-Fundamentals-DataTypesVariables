93e4f3b862afc861a7de05e6450276a6eba0600d
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 22:48:40 +0300

    Add files via upload

---
+++ CharsToString/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CharsToString
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            char one = char.Parse(Console.ReadLine());
+            char two = char.Parse(Console.ReadLine());
+            char three = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{one}{two}{three}") ;
+        }
+    }
+}
