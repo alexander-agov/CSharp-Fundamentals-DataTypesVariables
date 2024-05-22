d8eec4c3d32fb775aa26aca4341a539099ca13c3
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:39:06 +0300

    Add files via upload

---
+++ RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RefactorSpecialNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+           
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfDigit = 0;
+                int digit = i;
+             
+                while (digit > 0)
+                {
+                    sumOfDigit += digit % 10;
+                    digit = digit / 10;
+                }
+                if (sumOfDigit == 5 || sumOfDigit == 7 || sumOfDigit == 11)
+                {
+                    Console.WriteLine($"{i} -> True");
+                }
+                else
+                {
+                    Console.WriteLine($"{i} -> False");
+                }
+            }
+        }
+    }
+}
+
+
