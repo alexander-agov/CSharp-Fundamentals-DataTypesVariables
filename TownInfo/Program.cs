640b2fd953d5558ab5ecf205595bc74a3e5e9339
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 00:27:32 +0300

    Add files via upload

---
+++ TownInfo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TownInfo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string name = (Console.ReadLine());    
+            uint population =uint.Parse(Console.ReadLine());    
+            ushort area = ushort.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {name} has population of {population} and area {area} square km.");
+        }
+    }
+}
