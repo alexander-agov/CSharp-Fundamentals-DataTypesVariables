291f546a4ed546e9f645b8d67c1b06b61a1080f9
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 22:38:04 +0300

    Add files via upload

---
+++ BeerKegs/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BeerKegs
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            double maxVolume = double.MinValue;
+            string perfectModel = string.Empty;
+            for (int i = 1; i <= n; i++)
+            {
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double volume = Math.PI * Math.Pow(radius, 2) * height;
+                if (volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    perfectModel = model;
+                }
+            }
+            Console.WriteLine(perfectModel);
+        }
+    }
+}
