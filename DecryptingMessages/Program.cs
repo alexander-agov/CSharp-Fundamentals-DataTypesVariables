b0f096a32cb00e18e6938fab15b10594ec825d93
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:09:29 +0300

    Add files via upload

---
+++ DecryptingMessages/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace DecryptingMessages
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int key = int.Parse(Console.ReadLine());
+            int numLines = int.Parse(Console.ReadLine());
+            string str = "";
+            for (int i = 1; i <= numLines; i++) 
+            {
+                char.TryParse(Console.ReadLine(), out char letter);
+               int  code = key + (int)letter;
+                str += (char)code;
+            }
+            Console.WriteLine(str);
+        }
+    }
+}
