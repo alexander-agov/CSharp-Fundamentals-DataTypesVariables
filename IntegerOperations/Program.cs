02480f1cbc6b3a30d465f6babb81421a36cd8f26
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:25:56 +0300

    Add files via upload

---
+++ IntegerOperations/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace IntegerOperations
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int one = int.Parse(Console.ReadLine());
+            int two = int.Parse(Console.ReadLine());
+            int three = int.Parse(Console.ReadLine());
+            int four = int.Parse(Console.ReadLine());
+            int result = (one + two) / three * four;
+            Console.WriteLine($"{result}");
+        }
+    }
+}
