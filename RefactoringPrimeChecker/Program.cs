5f945e45a3589a4fe052dc1d68020845d6504438
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:36:44 +0300

    Add files via upload

---
+++ RefactoringPrimeChecker/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace RefactoringPrimeChecker
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int num = 2; num <= n; num++)
+            {
+                bool isPrime = true;
+                for (int divider = 2; divider < num; divider++)
+                {
+                    if (num % divider == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                Console.WriteLine("{0} -> {1}", num, isPrime.ToString().ToLower());
+            }
+        }
+    }
+}
+
