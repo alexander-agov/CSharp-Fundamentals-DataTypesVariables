bfb785dec4b890b7782b011f9e02240d9c5acdbf
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 00:33:34 +0300

    Add files via upload

---
+++ WaterOverflow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+
+namespace WaterOverflow
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sumOfLiters = 0;
+            for (int i = 1; i <= num; i++)
+            {
+                int liters = int.Parse(Console.ReadLine());
+                sumOfLiters += liters;
+                int tankCapacity = 255;
+                if (sumOfLiters > tankCapacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    sumOfLiters -= liters;
+                    continue;
+                }
+            }
+            Console.WriteLine(sumOfLiters);
+        }
+    }
+}
+
