5ecee1042baf3699d195f89fbe24c06e9c8e9d75
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:02:13 +0300

    Add files via upload

---
+++ DataTypeFinder/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace DataTypeFinder
+{
+    internal class Program
+    {
+        static void Main()
+        {
+           string input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (int.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                    input = Console.ReadLine();
+                }
+                else if (float.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                    input = Console.ReadLine();
+                }
+                else if (bool.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                    input = Console.ReadLine();
+                }
+                else if (char.TryParse(input, out _))
+                {
+                    Console.WriteLine($"{input} is character type");
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                    input = Console.ReadLine();
+                }
+            }
+        }
+    }
+}
