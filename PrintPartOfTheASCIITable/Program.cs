f0e6ebee4933a54f86675c019992ded4255727d6
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:36:16 +0300

    Add files via upload

---
+++ PrintPartOfTheASCIITable/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PrintPartOfTheASCIITable
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int firstCharIndex = int.Parse(Console.ReadLine());
+            int secondCharIndex = int.Parse(Console.ReadLine());
+            for (int i = firstCharIndex; i <= secondCharIndex; i++)
+            {
+                Console.Write((char) i + " ") ;
+            }
+        }
+    }
+}
