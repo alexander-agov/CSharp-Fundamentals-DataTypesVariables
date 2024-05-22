f3ffcd3aabf7f7a141fe65306cc940ca354cfad3
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:29:18 +0300

    Add files via upload

---
+++ Pokemon/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Pokemon
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int pokePower = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustionFactor = int.Parse(Console.ReadLine());
+            int count = 0;
+            double pokeP = pokePower;
+            while (pokePower >= distance)
+            {
+                pokePower -= distance;
+                
+                count++;
+                if (pokePower < distance)
+                {
+                    Console.WriteLine($"{pokePower}\n{count}");
+                    return;
+                }
+                if (pokeP / 2.0 == pokePower && exhaustionFactor != 0)
+                {
+                   pokePower= pokePower / exhaustionFactor;
+                    
+                }
+            }
+            Console.WriteLine($"{pokePower}\n{count}");
+        }
+    }
+}
+    
+
+
+
