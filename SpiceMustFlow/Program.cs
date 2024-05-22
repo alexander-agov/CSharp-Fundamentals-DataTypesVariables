f6677ef87415665cb90ec067bc3ba6dda6f50b93
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 00:25:13 +0300

    Add files via upload

---
+++ SpiceMustFlow/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel;
+
+namespace SpiceMustFlow
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int count = 0;
+            int newLeave = 0;
+            while (startingYield >= 100)
+            {
+                count++;
+                newLeave += startingYield - 26;
+                startingYield -= 10;
+                if (startingYield < 100)
+                {
+                    newLeave -= 26;
+                }
+            }
+            Console.WriteLine($"{count}\n{(newLeave)}");
+        }
+    }
+}
+
+
