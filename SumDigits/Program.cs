6507335d8fc8636a65a88c51c7cf59c07520be43
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 00:25:49 +0300

    Add files via upload

---
+++ SumDigits/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace SumDigits
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int digit = num;
+            int sum = 0;
+            while (digit > 0)
+            {
+                sum += digit % 10;
+                digit = digit / 10;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
