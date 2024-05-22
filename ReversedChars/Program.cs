ec803a1edbd8e0d29ac6f20997366f56f07d5c2e
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 00:17:35 +0300

    Add files via upload

---
+++ ReversedChars/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ReversedChars
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            char firstChar = char.Parse(Console.ReadLine());
+            char secondChar = char.Parse(Console.ReadLine());
+            char thirdChar = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{thirdChar} {secondChar} { firstChar}");
+        }
+    }
+}
