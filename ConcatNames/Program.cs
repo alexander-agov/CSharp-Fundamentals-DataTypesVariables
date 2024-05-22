e67b0bb33cf3e271f25245c5a4b7814ebfc39cd8
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 22:58:29 +0300

    Add files via upload

---
+++ ConcatNames/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ConcatNames
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string delimiter = Console.ReadLine();
+            string result = firstName + delimiter + lastName;
+            Console.WriteLine(result);
+        }
+    }
+}
