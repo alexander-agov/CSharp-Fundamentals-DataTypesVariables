b603a9f49cc7d82cc3347c4d7942b989b9f00ad4
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:11:43 +0300

    Add files via upload

---
+++ Elevator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Elevator
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            if (people <= capacity)
+            {
+                Console.WriteLine("1");
+            }
+            else if (people % capacity == 0)
+            {
+                int courses = 0;
+                courses = people / capacity;
+                Console.WriteLine($"{courses}");
+            }
+            else if (people % capacity <= capacity)
+            {
+                int courses = 0;
+                courses = (int)Math.Ceiling((double)people / capacity);
+                Console.WriteLine($"{courses}");
+            }
+        }
+    }
+}
+
