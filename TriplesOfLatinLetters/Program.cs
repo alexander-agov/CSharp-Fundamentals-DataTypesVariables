fa228d6c5feec7a60e514677a8a3864807cfa72e
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-23 00:32:44 +0300

    Add files via upload

---
+++ TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace TriplesOfLatinLetters
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    for (int k = 0; k < n; k++)
+                    {
+                        char firstChar = (char)(97 + i);
+                        char secondChar = (char)(97 + j);
+                        char thirdChar = (char)(97 + k);
+                        Console.WriteLine($"{firstChar}{secondChar}{thirdChar}");
+                    }
+                }
+            }
+        }
+    }
+}
