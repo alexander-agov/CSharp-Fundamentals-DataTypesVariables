61063dc2c8adc15780980c8e2ec6f167caea9d99
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:13:05 +0300

    Add files via upload

---
+++ FloatingEquality/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace FloatingEquality
+{
+    internal class Program
+    {
+            static void Main()
+            {
+                double eps = 0.000001; 
+
+                if (!double.TryParse(Console.ReadLine(), out double number1))
+                {
+                    Console.WriteLine("Invalid input for the first number.");
+                    return;
+                }
+
+                if (!double.TryParse(Console.ReadLine(), out double number2))
+                {
+                    Console.WriteLine("Invalid input for the second number.");
+                    return;
+                }
+
+                if (Math.Abs(number1 - number2) < eps)
+                {
+                    Console.WriteLine("True ");
+                }
+                else
+                {
+                    Console.WriteLine("False ");
+                }
+            }
+        }
+    }
+
