d8b3e82220c99da82c25c9da85c10d16eff95056
Alexander Agov <164613746+alexander-agov@users.noreply.github.com>
2024-05-22 23:12:25 +0300

    Add files via upload

---
+++ ExactSumOfRealNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ExactSumOfRealNumbers
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());           //1000000000000000015
+            decimal sum = 0;
+            for (int i = 0; i < n; i++)
+                sum += decimal.Parse(Console.ReadLine());
+            Console.WriteLine(sum);
+        }
+    }
+}
+
